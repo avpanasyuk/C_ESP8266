23f85a016c205a799b93368fe4f5fd6f4ae19345
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2023-05-29 22:35:32 -0400

    Replaced HTTP_GET macro with functions

--- General.h
+++ General.h
@@ -1,37 +1,18 @@
 #pragma once
 
-#if defined(AVP_SECURE_GET)
 #include <WiFiClientSecure.h>
-#else
-#include <WiFiClient.h>
-#endif
 #include <../C_General/Error.h>
 
 namespace avp {
+  /**
+   *@brief sending GET request
+   *
+   * @param Message - like "/pin?i=5&set=1"
+   */
+  const char *SendGET(const char *server, String Message, uint16_t port = 80, unsigned long Timeout = 5000);
+  const char *SendGET_Secure(const char *server, String Message, uint16_t port = 443, unsigned long Timeout = 5000);
   // ***************** INTERNET CLIENT CONNECTION ************************************
-#if defined(AVP_SECURE_GET)
-  extern WiFiClientSecure client; // DO NOT FORGET TO PUT avp::client.setInsecure(); at 
-  // the end of setup()
-  static constexpr uint16_t ServerPort = 443;
-#else
-  extern WiFiClient client;
-  static constexpr uint16_t ServerPort = 80;
-#endif
-
-  extern bool GET_succeed;
-  extern String GET_responce;
-
-  void FinishTalk();
-
-  void StopClient();
-
   const String GenerateHTML(const String &html_body, uint16_t AutoRefresh_s = 0, const char *title = nullptr);
 } // namespace avp
-
-#define HTML_GET_PRINTF(server, format,...) do{ avp::GET_succeed = false; \
-  if(avp::client.connect(server, avp::ServerPort)) {\
-    if(avp::client.printf("GET "  format " HTTP/1.1\r\nHost: %s\r\nUser-Agent: (panasyuk@yahoo.com)\r\nConnection: close\r\n\r\n", \
-      ##__VA_ARGS__, server) > 0) avp::FinishTalk(); else  avp::StopClient(); \
-  } else Serial.println("HTML_GET_PRINTF connect failed!"); }while(0)
 
 
--- common_esp.cpp
+++ common_esp.cpp
@@ -1,38 +1,78 @@
+#define _GNU_SOURCE
+#include <cstring>
 #include <../C_ESP/General.h>
 
 namespace avp {
-#if defined(AVP_SECURE_GET)
-  WiFiClientSecure client;
-#else
-  WiFiClient client;
-#endif
-  bool GET_succeed = false;
-  String GET_responce;
-
-  void FinishTalk() {
-    GET_succeed = true;  GET_responce.clear();
-    // we are waiting until either response reaches or server closes connection
-    while(client.connected() && client.available() == 0) delay(1);
-    // if we have response let's read it
-    while(client.available()) GET_responce += char(client.read());
-    // if server closed connection, that't it
-    if(!client.connected()) client.stop();
-  } // FinishTalk
-
-  void StopClient() {
-    delay(100);
-    client.stop();
-    debug_puts("HTML_GET_PRINTF send Failed!");
-    while(client.connected()) delay(0);
-  } // StopClient
-
   const String GenerateHTML(const String &html_body, uint16_t AutoRefresh_s, const char *title) {
-    String out = "<!DOCTYPE html><html><head>";
+    String out = F("<!DOCTYPE html><html><head>");
     if(title != nullptr) out += "<title>" + String(title) + "</title>";
     if(AutoRefresh_s != 0)
-      out += "<meta http-equiv=\"refresh\" content=\"" + String(AutoRefresh_s) + "\">";
+      out += F("<meta http-equiv=\"refresh\" content=\"") + String(AutoRefresh_s) + "\">";
     out += "</head><body>" + html_body + "</body></html>";
     return out;
   }  // GenerateAutoRefreshHTML
+
+/**
+ *@brief sending GET request
+ *
+ * @param Message - like "/pin?i=5&set=1"
+ */
+  static const char *SendGET_(WiFiClient *pClient, const char *server, String Message, uint16_t port) {// --------------- CHECK PUMP POWER ------------------------------
+    static String GET_responce;
+
+    if(pClient->connect(server, port)) {
+      pClient->print("GET " + Message + F(" HTTP/1.1\r\nHost: ") + server + F("\r\nConnection: close\r\n\r\n"));
+      // we are waiting until either response reaches or server closes connection
+#if 0
+      while(pClient->connected() && pClient->available() == 0) delay(1);
+      // if we have response let's read it
+      GET_responce.reserve(pClient->available());
+      while(pClient->available()) GET_responce += char(pClient->read());
+      // if server closed connection, that't it
+#endif
+      auto Timeout = millis() + 10000;
+      GET_responce.clear();
+      while(pClient->connected()) {
+        while(pClient->available()) GET_responce += char(pClient->read());
+        if(millis() > Timeout) {
+          GET_responce.clear();
+          break;
+        }
+      }
+
+      pClient->stop();
+      static const char *cl = "content-length:";
+      const char *r = GET_responce.c_str();
+      const char *p = strcasestr(r, cl);
+      if(p != nullptr) {
+        p += strlen(cl);
+        
+        unsigned int Length;
+        if(sscanf(p, "%u", &Length) == 1) {
+          // debug_printf("%s<br>%u<br>%s", r, Length, r + (strlen(r) - Length));
+          return r + (strlen(r) - Length);
+        }  else debug_printf("Failed to parse '%s'!", p);
+      } else debug_printf("Cannot find '%s' in '%s' (case ignored)", cl, r);
+    } else debug_printf("Failed to connect to '%s'!", server);
+    return nullptr;
+  } // CheckPump
+
+/**
+ *@brief sending GET request
+ *
+ * @param Message - like "/pin?i=5&set=1"
+ */
+  const char *SendGET(const char *server, String Message, uint16_t port, unsigned long Timeout) {
+    static WiFiClient c;
+    c.setTimeout(Timeout);
+    return SendGET_(&c, server, Message, port);
+  } // SendGET
+
+  const char *SendGET_Secure(const char *server, String Message, uint16_t port, unsigned long Timeout) {
+    static WiFiClientSecure c;
+    c.setInsecure();
+    c.setTimeout(Timeout);
+    return SendGET_(&c, server, Message, port);
+  } // SendGET_Secure
 }
 
