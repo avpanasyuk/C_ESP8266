9e980a4ca867fd47115d6eb3d6282b361f74e981
Alexander Panasyuk <alexander.panasyuk@gmail.com>
2023-06-10 14:54:19 -0400

    Minor cleanups

--- General.h
+++ General.h
@@ -12,10 +12,10 @@ namespace avp {
    * @retval pointer to a static string which contains return message with headers stripped. It will be modified on the next call
    *    or nullptr if request was unsuccessful
    */
-  const char *SendGET(const char *server, const String &Message, uint16_t port = 80, unsigned long Timeout = DefaultTimeout_ms);
-  const char *SendGET_Secure(const char *server, const String &Message, uint16_t port = 443, unsigned long Timeout = DefaultTimeout_ms);
+  const char *SendGET(const char *server, const char *Message, uint16_t port = 80, unsigned long Timeout = DefaultTimeout_ms);
+  const char *SendGET_Secure(const char *server, const char *Message, uint16_t port = 443, unsigned long Timeout = DefaultTimeout_ms);
   // ***************** INTERNET CLIENT CONNECTION ************************************
-  const String &GenerateHTML(const String &html_body, uint16_t AutoRefresh_s = 0, const char *title = nullptr);
+  const String &GenerateHTML(const char *html_body, uint16_t AutoRefresh_s = 0, const char *title = nullptr);
 } // namespace avp
 
 
--- common_esp.cpp
+++ common_esp.cpp
@@ -1,16 +1,33 @@
 #define _GNU_SOURCE
 #include <cstring>
+#include <ESP8266WiFi.h>
+#include "C_General\General_C.h"
+#include "C_ARDUINO\General.h"
 #include "C_ESP\General.h"
 
 namespace avp {
-  const String &GenerateHTML(const String &html_body, uint16_t AutoRefresh_s, const char *title) {
+  const String &GenerateHTML(const char *html_body, uint16_t AutoRefresh_s, const char *title) {
     static String out;
+    if(!out) out.reserve(6000);
     out.clear();
     out += F("<!DOCTYPE html><html><head>");
-    if(title != nullptr) out += "<title>" + String(title) + "</title>";
-    if(AutoRefresh_s != 0)
-      out += "<meta http-equiv=\"refresh\" content=\"" + String(AutoRefresh_s) + "\">";
-    out += "</head><body>" + html_body + "</body></html>";
+    if(title != nullptr) {
+      out += "<title>";
+      out += title;
+      out += "</title>";
+    }
+    if(AutoRefresh_s != 0) {
+      out += F("<meta http-equiv=\"refresh\" content=\"");
+      out += AutoRefresh_s;
+      out += "\">";
+    }
+    out += "</head><body>";
+    // out += avp::urlencode(html_body);
+    out += html_body;
+    out += "</body></html>";
+  #ifdef DEBUG
+    // Serial.println(out);
+  #endif
     return out;
   }  // GenerateAutoRefreshHTML
 
@@ -19,12 +36,16 @@ namespace avp {
  *
  * @param Message - like "/pin?i=5&set=1"
  */
-  static const char *SendGET_(WiFiClient *pClient, const char *server, const String &Message, uint16_t port,
+  static const char *SendGET_(WiFiClient *pClient, const char *server, const char *Message, uint16_t port,
     uint32_t Timeout_ms) {
     static String GET_responce;
+    pClient->setTimeout(Timeout_ms);
 
-    if(pClient->connect(server, port)) {
-      pClient->printf("GET %s HTTP/1.1\r\nHost: %s\r\nUser-Agent: ff\r\nConnection: close\r\n\r\n", Message.c_str(), server);
+    IPAddress remote_addr;
+    if(!WiFi.hostByName(server, remote_addr, Timeout_ms))
+      debug_printf("WiFi.hostByName for host %s failed!\n", server);
+    else if(pClient->connect(remote_addr, port)) {
+      pClient->printf("GET %s HTTP/1.1\r\nHost: %s\r\nUser-Agent: ff\r\nConnection: close\r\n\r\n", Message, server);
       
       // we are waiting until either response reaches or server closes connection
       auto Timeout = millis() + Timeout_ms;
@@ -55,16 +76,14 @@ namespace avp {
  *
  * @param Message - like "/pin?i=5&set=1"
  */
-  const char *SendGET(const char *server, const String &Message, uint16_t port, unsigned long Timeout) {
+  const char *SendGET(const char *server, const char *Message, uint16_t port, unsigned long Timeout) {
     static WiFiClient c;
-    c.setTimeout(Timeout);
     return SendGET_(&c, server, Message, port, Timeout);
   } // SendGET
 
-  const char *SendGET_Secure(const char *server, const String &Message, uint16_t port, unsigned long Timeout) {
+  const char *SendGET_Secure(const char *server, const char *Message, uint16_t port, unsigned long Timeout) {
     static WiFiClientSecure c;
     c.setInsecure();
-    c.setTimeout(Timeout);
     return SendGET_(&c, server, Message, port, Timeout);
   } // SendGET_Secure
 }
